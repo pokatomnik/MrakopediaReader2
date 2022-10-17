6047f7100d126237f08aa622d8411dea6d0297c5
Danilian Akhmedzianov <pokatomnik@yandex.ru>
2022-10-17 10:13:46 +0300

    Make RadioButton label clickable

--- app/src/main/java/tk/pokatomnik/mrakopediareader2/screens/settings/Settings.kt
+++ app/src/main/java/tk/pokatomnik/mrakopediareader2/screens/settings/Settings.kt
@@ -2,12 +2,14 @@ package tk.pokatomnik.mrakopediareader2.screens.settings
 
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.selection.selectable
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.RadioButton
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.semantics.Role
 import androidx.compose.ui.unit.dp
 import tk.pokatomnik.mrakopediareader2.services.preferences.global.ThemeIdentifier
 import tk.pokatomnik.mrakopediareader2.services.preferences.global.rememberThemeIdentifier
@@ -32,33 +34,57 @@ fun Settings() {
                 .padding(top = 16.dp)) {
                 Text("Тема")
                 Row(
-                    modifier = Modifier.fillMaxWidth(),
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .selectable(
+                            selected = themeIdentifierState.value == ThemeIdentifier.AUTO,
+                            onClick = { themeIdentifierState.value = ThemeIdentifier.AUTO },
+                            role = Role.RadioButton
+                        )
+                        .padding(vertical = 8.dp),
                     verticalAlignment = Alignment.CenterVertically
                 ) {
                     RadioButton(
                         selected = themeIdentifierState.value == ThemeIdentifier.AUTO,
-                        onClick = { themeIdentifierState.value = ThemeIdentifier.AUTO }
+                        onClick = null
                     )
+                    Spacer(modifier = Modifier.width(4.dp))
                     Text(text = "Как в системе")
                 }
                 Row(
-                    modifier = Modifier.fillMaxWidth(),
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .selectable(
+                            selected = themeIdentifierState.value == ThemeIdentifier.LIGHT,
+                            onClick = { themeIdentifierState.value = ThemeIdentifier.LIGHT },
+                            role = Role.RadioButton
+                        )
+                        .padding(vertical = 8.dp),
                     verticalAlignment = Alignment.CenterVertically
                 ) {
                     RadioButton(
                         selected = themeIdentifierState.value == ThemeIdentifier.LIGHT,
-                        onClick = { themeIdentifierState.value = ThemeIdentifier.LIGHT }
+                        onClick = null
                     )
+                    Spacer(modifier = Modifier.width(4.dp))
                     Text(text = "Светлая")
                 }
                 Row(
-                    modifier = Modifier.fillMaxWidth(),
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .selectable(
+                            selected = themeIdentifierState.value == ThemeIdentifier.DARK,
+                            onClick = { themeIdentifierState.value = ThemeIdentifier.DARK },
+                            role = Role.RadioButton
+                        )
+                        .padding(vertical = 8.dp),
                     verticalAlignment = Alignment.CenterVertically
                 ) {
                     RadioButton(
                         selected = themeIdentifierState.value == ThemeIdentifier.DARK,
-                        onClick = { themeIdentifierState.value = ThemeIdentifier.DARK }
+                        onClick = null
                     )
+                    Spacer(modifier = Modifier.width(4.dp))
                     Text(text = "Темная")
                 }
             }

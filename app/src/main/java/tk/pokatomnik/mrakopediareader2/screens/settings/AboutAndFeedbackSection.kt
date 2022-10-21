7c574586e66a42618b7819bad0bea9b169d7cf1c
Danilian Akhmedzianov <pokatomnik@yandex.ru>
2022-10-22 01:19:06 +0300

    Add about info and feedback buttons

---
+++ app/src/main/java/tk/pokatomnik/mrakopediareader2/screens/settings/AboutAndFeedbackSection.kt
@@ -0,0 +1,55 @@
+package tk.pokatomnik.mrakopediareader2.screens.settings
+
+import android.content.Intent
+import android.content.Intent.ACTION_VIEW
+import android.net.Uri
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.Text
+import androidx.compose.material.TextButton
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun AboutAndFeedbackSection() {
+    val context = LocalContext.current
+
+    val handleFeedbackClick = {
+        val intent = Intent(ACTION_VIEW, Uri.parse("mailto:pokatomnik@yandex.ru"))
+        context.startActivity(intent)
+    }
+
+    val handleSourceCodeClick = {
+        val intent = Intent(ACTION_VIEW, Uri.parse("https://github.com/pokatomnik/MrakopediaReader2"))
+        context.startActivity(intent)
+    }
+
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(top = 16.dp)
+    ) {
+        Text("Приложение для чтения историй с сайта https://mrakopedia.net")
+        Spacer(modifier = Modifier.height(8.dp))
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.Center
+        ) {
+            TextButton(onClick = handleFeedbackClick) {
+                Text("Написать автору")
+            }
+        }
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.Center
+        ) {
+            TextButton(onClick = handleSourceCodeClick) {
+                Text("Исходный код")
+            }
+        }
+    }
+}
--- app/src/main/java/tk/pokatomnik/mrakopediareader2/screens/settings/Settings.kt
+++ app/src/main/java/tk/pokatomnik/mrakopediareader2/screens/settings/Settings.kt
@@ -1,25 +1,20 @@
 package tk.pokatomnik.mrakopediareader2.screens.settings
 
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.selection.selectable
 import androidx.compose.foundation.verticalScroll
-import androidx.compose.material.RadioButton
-import androidx.compose.material.Text
+import androidx.compose.material.Divider
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.semantics.Role
 import androidx.compose.ui.unit.dp
-import tk.pokatomnik.mrakopediareader2.services.preferences.global.ThemeIdentifier
-import tk.pokatomnik.mrakopediareader2.services.preferences.global.rememberThemeIdentifier
 import tk.pokatomnik.mrakopediareader2.ui.components.PageContainer
 import tk.pokatomnik.mrakopediareader2.ui.components.PageTitle
 
 @Composable
 fun Settings() {
-    val themeIdentifierState = rememberThemeIdentifier()
-    
     PageContainer(
         header = { PageTitle(title = "Настройки") }
     ) {
@@ -29,65 +24,9 @@ fun Settings() {
                 .padding(horizontal = 16.dp)
                 .verticalScroll(rememberScrollState())
         ) {
-            Column(modifier = Modifier
-                .fillMaxWidth()
-                .padding(top = 16.dp)) {
-                Text("Тема")
-                Row(
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .selectable(
-                            selected = themeIdentifierState.value == ThemeIdentifier.AUTO,
-                            onClick = { themeIdentifierState.value = ThemeIdentifier.AUTO },
-                            role = Role.RadioButton
-                        )
-                        .padding(vertical = 8.dp),
-                    verticalAlignment = Alignment.CenterVertically
-                ) {
-                    RadioButton(
-                        selected = themeIdentifierState.value == ThemeIdentifier.AUTO,
-                        onClick = null
-                    )
-                    Spacer(modifier = Modifier.width(4.dp))
-                    Text(text = "Как в системе")
-                }
-                Row(
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .selectable(
-                            selected = themeIdentifierState.value == ThemeIdentifier.LIGHT,
-                            onClick = { themeIdentifierState.value = ThemeIdentifier.LIGHT },
-                            role = Role.RadioButton
-                        )
-                        .padding(vertical = 8.dp),
-                    verticalAlignment = Alignment.CenterVertically
-                ) {
-                    RadioButton(
-                        selected = themeIdentifierState.value == ThemeIdentifier.LIGHT,
-                        onClick = null
-                    )
-                    Spacer(modifier = Modifier.width(4.dp))
-                    Text(text = "Светлая")
-                }
-                Row(
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .selectable(
-                            selected = themeIdentifierState.value == ThemeIdentifier.DARK,
-                            onClick = { themeIdentifierState.value = ThemeIdentifier.DARK },
-                            role = Role.RadioButton
-                        )
-                        .padding(vertical = 8.dp),
-                    verticalAlignment = Alignment.CenterVertically
-                ) {
-                    RadioButton(
-                        selected = themeIdentifierState.value == ThemeIdentifier.DARK,
-                        onClick = null
-                    )
-                    Spacer(modifier = Modifier.width(4.dp))
-                    Text(text = "Темная")
-                }
-            }
+            ThemeSettingsSection()
+            Divider(modifier = Modifier.fillMaxWidth())
+            AboutAndFeedbackSection()
         }
     }
 }

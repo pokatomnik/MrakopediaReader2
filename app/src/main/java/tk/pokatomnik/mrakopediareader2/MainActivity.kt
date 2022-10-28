abb8f3e1439938343fc690286d131c563392482f
Danilian Akhmedzianov <pokatomnik@yandex.ru>
2022-10-29 00:51:35 +0300

    Rewrite navigation and fix page scrolling percent bug

--- app/src/main/java/tk/pokatomnik/mrakopediareader2/MainActivity.kt
+++ app/src/main/java/tk/pokatomnik/mrakopediareader2/MainActivity.kt
@@ -8,105 +8,56 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.*
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
-import androidx.navigation.NavDestination
-import androidx.navigation.NavDestination.Companion.hierarchy
-import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
-import androidx.navigation.compose.currentBackStackEntryAsState
-import androidx.navigation.compose.rememberNavController
 import dagger.hilt.android.AndroidEntryPoint
 import tk.pokatomnik.mrakopediareader2.screens.categories.Categories
 import tk.pokatomnik.mrakopediareader2.screens.favorites.Favorites
 import tk.pokatomnik.mrakopediareader2.screens.history.History
 import tk.pokatomnik.mrakopediareader2.screens.search.Search
 import tk.pokatomnik.mrakopediareader2.screens.settings.Settings
-import tk.pokatomnik.mrakopediareader2.screens.story.Story
 import tk.pokatomnik.mrakopediareader2.screens.stories.Stories
+import tk.pokatomnik.mrakopediareader2.screens.story.Story
 import tk.pokatomnik.mrakopediareader2.services.index.rememberMrakopediaIndex
+import tk.pokatomnik.mrakopediareader2.services.navigation.rememberNavigation
 import tk.pokatomnik.mrakopediareader2.ui.components.BottomNavItem
-import tk.pokatomnik.mrakopediareader2.ui.components.rememberToast
 import tk.pokatomnik.mrakopediareader2.ui.theme.MrakopediaReader2Theme
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
-    private fun NavHostController.navigateSingle(route: String) {
-        navigate(route) {
-            launchSingleTop = true
-        }
-    }
-
-    private fun NavDestination?.on(route: String): Boolean {
-        return this?.hierarchy?.any { it.route == route } == true
-    }
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            val showToast = rememberToast()
             val mrakopediaIndex = rememberMrakopediaIndex()
-            val (categoryTitle, setCategoryTitle) = remember { mutableStateOf<String?>(null) }
-            val (pageTitle, setPageTitle) = remember { mutableStateOf<String?>(null) }
-
+            val navigation = rememberNavigation()
             MrakopediaReader2Theme {
-                val navController = rememberNavController()
                 Scaffold(
                     bottomBar = {
-                        BottomNavigation  {
-                            val navBackStackEntry by navController.currentBackStackEntryAsState()
-                            val currentDestination = navBackStackEntry?.destination
-
+                        BottomNavigation {
                             BottomNavItem(
                                 icon = Icons.Filled.List,
                                 title = "Категории",
-                                selected = currentDestination.on("categories"),
-                                onClick = { navController.navigateSingle("categories") }
-                            )
-                            BottomNavItem(
-                                icon = Icons.Filled.FileCopy,
-                                title = "Список Историй",
-                                selected = currentDestination.on("stories"),
-                                onClick = {
-                                    if (categoryTitle != null) {
-                                        navController.navigateSingle("stories")
-                                    } else {
-                                        showToast("Выберите категорию")
-                                    }
-                                }
-                            )
-                            BottomNavItem(
-                                icon = Icons.Filled.AutoStories,
-                                title = "История",
-                                selected = currentDestination.on("story"),
-                                onClick = {
-                                    if (pageTitle != null) {
-                                        navController.navigateSingle("story")
-                                    } else {
-                                        showToast("Выберите историю")
-                                    }
-                                }
+                                selected = navigation.onCategories(),
+                                onClick = { navigation.navigateToCategories() }
                             )
                             BottomNavItem(
                                 icon = Icons.Filled.Favorite,
                                 title = "Избранное",
-                                selected = currentDestination.on("favorites"),
-                                onClick = { navController.navigateSingle("favorites") }
+                                selected = navigation.onFavorites(),
+                                onClick = { navigation.navigateToFavorites() }
                             )
                             BottomNavItem(
                                 icon = Icons.Filled.History,
                                 title = "История",
-                                selected = currentDestination.on("history"),
-                                onClick = { navController.navigateSingle("history") }
+                                selected = navigation.onHistory(),
+                                onClick = { navigation.navigateToHistory() }
                             )
                             BottomNavItem(
                                 icon = Icons.Filled.Settings,
                                 title = "Настройки",
-                                selected = currentDestination.on("settings"),
-                                onClick = { navController.navigateSingle("settings") }
+                                selected = navigation.onSettings(),
+                                onClick = { navigation.navigateToSettings() }
                             )
                         }
                     }
@@ -116,86 +67,86 @@ class MainActivity : ComponentActivity() {
                         color = MaterialTheme.colors.background
                     ) {
                         NavHost(
-                            navController = navController,
-                            startDestination = "categories",
+                            navController = navigation.navController,
+                            startDestination = navigation.categoriesRoute,
                             modifier = Modifier.padding(innerPadding)
                         ) {
-                            composable(route = "categories") {
+                            composable(route = navigation.categoriesRoute) {
                                 Categories(
-                                    onSelectCategoryTitle = {
-                                        setCategoryTitle(it)
-                                        setPageTitle(null)
-                                        navController.navigateSingle("stories")
+                                    onSelectCategoryTitle = { categoryTitle ->
+                                        navigation.navigateToStories(categoryTitle)
                                     },
-                                    onPressSearch = { navController.navigateSingle("search") }
+                                    onPressSearch = { navigation.navigateToSearch() }
                                 )
                             }
-                            composable(route = "search") {
+                            composable(route = navigation.searchRoute) {
                                 Search(
-                                    onSelectPage = {
-                                        setCategoryTitle(mrakopediaIndex.getGeneralCategoryTitle())
-                                        setPageTitle(it)
-                                        navController.navigateSingle("story")
+                                    onSelectPage = { storyTitle ->
+                                        navigation.navigateToStory(
+                                            mrakopediaIndex.getGeneralCategoryTitle(),
+                                            storyTitle
+                                        )
                                     },
-                                    onSelectCategory = {
-                                        setCategoryTitle(it)
-                                        setPageTitle(null)
-                                        navController.navigateSingle("stories")
+                                    onSelectCategory = { categoryTitle ->
+                                        navigation.navigateToStories(categoryTitle)
                                     }
                                 )
                             }
-                            composable(route = "stories") {
-                                if (categoryTitle != null) {
-                                    Stories(
-                                        selectedCategoryTitle = categoryTitle,
-                                        onSelectPage = {
-                                            setPageTitle(it)
-                                            navController.navigateSingle("story")
-                                        }
-                                    )
-                                }
+                            composable(route = navigation.storiesRoute) {
+                                val categoryTitle = navigation.getCategoryTitle(it)
+                                    ?: mrakopediaIndex.getGeneralCategoryTitle()
+                                Stories(
+                                    selectedCategoryTitle = categoryTitle,
+                                    onSelectPage = { storyTitle ->
+                                        navigation.navigateToStory(
+                                            categoryTitle = categoryTitle,
+                                            storyTitle = storyTitle
+                                        )
+                                    }
+                                )
                             }
-                            composable(route = "story") {
-                                if (pageTitle != null && categoryTitle != null) {
-                                    Story(
-                                        selectedCategoryTitle = categoryTitle,
-                                        selectedPageTitle = pageTitle,
-                                        onNavigateToPage = {
-                                            setCategoryTitle(mrakopediaIndex.getGeneralCategoryTitle())
-                                            setPageTitle(it)
-                                        },
-                                        onNavigateToCategory = {
-                                            setCategoryTitle(it)
-                                            setPageTitle(null)
-                                            navController.navigateSingle("stories")
-                                        }
-                                    )
-                                }
+                            composable(route = navigation.storyRoute) {
+                                val categoryTitle = navigation.getCategoryTitle(it)
+                                    ?: mrakopediaIndex.getGeneralCategoryTitle()
+                                val pageTitle = navigation.getStoryTitle(it) ?: ""
+                                Story(
+                                    selectedCategoryTitle = categoryTitle,
+                                    selectedPageTitle = pageTitle,
+                                    onNavigateToPage = { storyTitle ->
+                                        navigation.navigateToStory(
+                                            categoryTitle = mrakopediaIndex.getGeneralCategoryTitle(),
+                                            storyTitle = storyTitle
+                                        )
+                                    },
+                                    onNavigateToCategory = { newCategoryTitle ->
+                                        navigation.navigateToStories(newCategoryTitle)
+                                    }
+                                )
                             }
-                            composable(route = "favorites") {
+                            composable(route = navigation.favoritesRoute) {
                                 Favorites(
-                                    onStoryClick = {
-                                        setCategoryTitle(mrakopediaIndex.getGeneralCategoryTitle())
-                                        setPageTitle(it)
-                                        navController.navigateSingle("story")
+                                    onStoryClick = { storyTitle ->
+                                        navigation.navigateToStory(
+                                            categoryTitle = mrakopediaIndex.getGeneralCategoryTitle(),
+                                            storyTitle = storyTitle
+                                        )
                                     },
-                                    onCategoryClick = {
-                                        setCategoryTitle(it)
-                                        setPageTitle(null)
-                                        navController.navigateSingle("stories")
+                                    onCategoryClick = { categoryTitle ->
+                                        navigation.navigateToStories(categoryTitle)
                                     }
                                 )
                             }
-                            composable(route = "history") {
+                            composable(route = navigation.historyRoute) {
                                 History(
-                                    onSelectPage = {
-                                        setCategoryTitle(mrakopediaIndex.getGeneralCategoryTitle())
-                                        setPageTitle(it)
-                                        navController.navigateSingle("story")
+                                    onSelectPage = { storyTitle ->
+                                        navigation.navigateToStory(
+                                            categoryTitle = mrakopediaIndex.getGeneralCategoryTitle(),
+                                            storyTitle = storyTitle
+                                        )
                                     }
                                 )
                             }
-                            composable(route = "settings") {
+                            composable(route = navigation.settingsRoute) {
                                 Settings()
                             }
                         }
---
+++ app/src/main/java/tk/pokatomnik/mrakopediareader2/services/navigation/Navigation.kt
@@ -0,0 +1,113 @@
+package tk.pokatomnik.mrakopediareader2.services.navigation
+
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.navigation.NavBackStackEntry
+import androidx.navigation.NavDestination
+import androidx.navigation.NavDestination.Companion.hierarchy
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.currentBackStackEntryAsState
+import androidx.navigation.compose.rememberNavController
+
+data class Navigation(
+    val navController: NavHostController,
+    private val serializer: Serializer
+) {
+    private fun NavHostController.navigateSingle(route: String) {
+        navigate(route) {
+            launchSingleTop = true
+        }
+    }
+
+    private fun NavDestination?.on(route: String): Boolean {
+        return this?.hierarchy?.any { it.route == route } == true
+    }
+
+    fun getCategoryTitle(entry: NavBackStackEntry): String? {
+        return entry.arguments?.getString(CATEGORY_TITLE_KEY)?.let {
+            serializer.parse(it)
+        }
+    }
+
+    fun getStoryTitle(entry: NavBackStackEntry): String? {
+        return entry.arguments?.getString(STORY_TITLE_KEY)?.let {
+            serializer.parse(it)
+        }
+    }
+
+    @Composable
+    private fun rememberCurrentDestination(): NavDestination? {
+        val navBackStackEntry by navController.currentBackStackEntryAsState()
+        return navBackStackEntry?.destination
+    }
+
+    val categoriesRoute = "/categories"
+    @Composable
+    fun onCategories(): Boolean {
+        val currentDestination = rememberCurrentDestination()
+        return currentDestination.on(categoriesRoute)
+    }
+    fun navigateToCategories() {
+        navController.navigateSingle(categoriesRoute)
+    }
+
+    val storiesRoute = "/categories/{$CATEGORY_TITLE_KEY}"
+    fun navigateToStories(categoryTitle: String) {
+        val serializedCategoryTitle = serializer.serialize(categoryTitle)
+        navController.navigateSingle("/categories/$serializedCategoryTitle")
+    }
+
+    val storyRoute = "/categories/{$CATEGORY_TITLE_KEY}/{$STORY_TITLE_KEY}"
+    fun navigateToStory(categoryTitle: String, storyTitle: String) {
+        val serializedCategoryTitle = serializer.serialize(categoryTitle)
+        val serializedStoryTitle = serializer.serialize(storyTitle)
+        navController.navigate("/categories/$serializedCategoryTitle/$serializedStoryTitle")
+    }
+
+    val historyRoute = "/history"
+    @Composable
+    fun onHistory(): Boolean {
+        val currentDestination = rememberCurrentDestination()
+        return currentDestination.on(historyRoute)
+    }
+    fun navigateToHistory() {
+        navController.navigateSingle(historyRoute)
+    }
+
+    val favoritesRoute = "/favorites"
+    @Composable
+    fun onFavorites(): Boolean {
+        val currentDestination = rememberCurrentDestination()
+        return currentDestination.on(favoritesRoute)
+    }
+    fun navigateToFavorites() {
+        navController.navigateSingle(favoritesRoute)
+    }
+
+    val settingsRoute = "/settings"
+    @Composable
+    fun onSettings(): Boolean {
+        val currentDestination = rememberCurrentDestination()
+        return currentDestination.on(settingsRoute)
+    }
+    fun navigateToSettings() {
+        navController.navigateSingle(settingsRoute)
+    }
+
+    val searchRoute = "/search"
+    fun navigateToSearch() {
+        navController.navigateSingle(searchRoute)
+    }
+
+    companion object {
+        private const val CATEGORY_TITLE_KEY = "categoryTitle"
+        private const val STORY_TITLE_KEY = "storyTitle"
+    }
+}
+
+@Composable
+fun rememberNavigation(): Navigation {
+    val navHostController = rememberNavController()
+    val serializer = rememberSerializer()
+    return Navigation(navHostController, serializer)
+}

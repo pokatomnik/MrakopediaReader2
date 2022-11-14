ed437970698fa6e33f69372077a8cbd1f87f596a
Danilian Akhmedzianov <pokatomnik@yandex.ru>
2022-11-14 13:17:08 +0300

    Categories section rework

--- app/src/main/java/tk/pokatomnik/mrakopediareader2/screens/story/Categories.kt
+++ app/src/main/java/tk/pokatomnik/mrakopediareader2/screens/story/Categories.kt
@@ -1,38 +1,41 @@
 package tk.pokatomnik.mrakopediareader2.screens.story
 
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
-import androidx.compose.material.Divider
-import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
+import androidx.compose.material.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.unit.dp
 import com.google.accompanist.flowlayout.FlowRow
+import com.google.accompanist.flowlayout.MainAxisAlignment
 
 @Composable
 fun Categories(
     categories: Collection<String>,
     onClick: (categoryTitle: String) -> Unit
 ) {
-    Spacer(modifier = Modifier.height(16.dp))
-    Divider(modifier = Modifier.fillMaxWidth())
-    Spacer(modifier = Modifier.height(8.dp))
-    Column(modifier = Modifier.fillMaxWidth()) {
-        Text(
-            text = "Категории:",
-            style = MaterialTheme.typography.h6
-        )
-    }
-    Spacer(modifier = Modifier.height(8.dp))
-    FlowRow(modifier = Modifier.fillMaxWidth()) {
-        for (categoryTitle in categories) {
-            ButtonRelated(
-                title = categoryTitle,
-                onClick = { onClick(categoryTitle) }
-            )
+    Spacer(
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(8.dp)
+    )
+    FlowRow(
+        modifier = Modifier.fillMaxSize(),
+        mainAxisAlignment = MainAxisAlignment.Center
+    ) {
+        categories.forEach {
+            TextButton(
+                onClick = { onClick(it) }
+            ) {
+                Text(
+                    text = "#$it",
+                    modifier = Modifier.alpha(0.7f)
+                )
+            }
         }
     }
 }

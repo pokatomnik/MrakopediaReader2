3fe1a56a7c0608a0877ffdcca57de98cdbeb4f49
Danilian Akhmedzianov <pokatomnik@yandex.ru>
2022-11-18 00:07:53 +0300

    Replace Divider with LinearProgressIndicator (#111)

--- app/src/main/java/tk/pokatomnik/mrakopediareader2/screens/story/ScrollPositionIndication.kt
+++ app/src/main/java/tk/pokatomnik/mrakopediareader2/screens/story/ScrollPositionIndication.kt
@@ -2,15 +2,10 @@ package tk.pokatomnik.mrakopediareader2.screens.story
 
 import androidx.compose.animation.core.animateFloatAsState
 import androidx.compose.foundation.ScrollState
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.material.Divider
-import androidx.compose.material.MaterialTheme
+import androidx.compose.material.LinearProgressIndicator
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.dp
 
 private fun Int.percentOf(max: Int): Int {
     return if (max == 0) 0 else 100 * this / max
@@ -18,20 +13,14 @@ private fun Int.percentOf(max: Int): Int {
 
 @Composable
 internal fun ScrollPositionIndication(scrollState: ScrollState) {
-    val withAnimated = animateFloatAsState(
+    val widthAnimated = animateFloatAsState(
         targetValue = scrollState
             .value
             .percentOf(scrollState.maxValue)
             .toFloat() / 100
     )
-    Row(
-        modifier = Modifier.fillMaxWidth()
-    ) {
-        Divider(
-            modifier = Modifier
-                .height(3.dp)
-                .fillMaxWidth(withAnimated.value)
-                .background(MaterialTheme.colors.secondary)
-        )
-    }
+    LinearProgressIndicator(
+        modifier = Modifier.fillMaxWidth(),
+        progress = widthAnimated.value
+    )
 }

04c19c464e2d76e95396c4bbd6167c45cfc3a83c
Danilian Akhmedzianov <pokatomnik@yandex.ru>
2022-11-24 21:20:07 +0300

    Remove images and share button

--- app/src/main/java/tk/pokatomnik/mrakopediareader2/domain/ImageInfo.kt
+++
@@ -1,6 +0,0 @@
-package tk.pokatomnik.mrakopediareader2.domain
-
-data class ImageInfo(
-    val imgPath: String,
-    val imgCaption: String?
-)
--- app/src/main/java/tk/pokatomnik/mrakopediareader2/services/readonlyparams/ReadonlyParametersComposable.kt
+++
@@ -1,9 +0,0 @@
-package tk.pokatomnik.mrakopediareader2.services.readonlyparams
-
-import androidx.compose.runtime.Composable
-import androidx.hilt.navigation.compose.hiltViewModel
-
-@Composable
-fun rememberReadonlyParameters(): ReadonlyParameters {
-    return hiltViewModel<ReadonlyParametersViewModel>().readonlyParameters
-}
